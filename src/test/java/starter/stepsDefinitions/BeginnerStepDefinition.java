3a0e71d7face665172d0e670cd5b389fdb3d5548
demmt3101 <dennismateo3101@gmail.com>
2026-03-13 12:35:29 -0500

    feat: implementa login basico con capturas en Serenity

--- src/test/java/starter/stepsDefinitions/BeginnerStepDefinition.java
+++ src/test/java/starter/stepsDefinitions/BeginnerStepDefinition.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.PageObject;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -15,33 +16,42 @@ public class BeginnerStepDefinition extends PageObject {
     By btnLogin = By.id("login-button");
     By lblProducts = By.xpath("//span[text()='Products']");
 
+    private static final int SHORT_WAIT = 1000;
+    private static final int MEDIUM_WAIT = 2000;
+
     @Given("que el usuario abre la pagina de login")
     public void queElUsuarioAbreLaPaginaDeLogin() throws InterruptedException {
         openUrl("https://www.saucedemo.com/");
-        Thread.sleep(2000);
+        Thread.sleep(MEDIUM_WAIT);
+        Serenity.takeScreenshot();
     }
 
     @When("ingresa el usuario {string}")
     public void ingresaElUsuario(String usuario) throws InterruptedException {
         $(txtUsername).waitUntilVisible().type(usuario);
-        Thread.sleep(1000);
+        Thread.sleep(SHORT_WAIT);
+        Serenity.takeScreenshot();
     }
 
     @And("ingresa la contraseña {string}")
     public void ingresaLaContrasena(String password) throws InterruptedException {
         $(txtPassword).waitUntilVisible().type(password);
-        Thread.sleep(1000);
+        Thread.sleep(SHORT_WAIT);
+        Serenity.takeScreenshot();
     }
 
     @And("hace clic en el boton login")
     public void haceClicEnElBotonLogin() throws InterruptedException {
         $(btnLogin).waitUntilClickable().click();
-        Thread.sleep(2000);
+        Thread.sleep(MEDIUM_WAIT);
+        Serenity.takeScreenshot();
     }
 
     @Then("deberia visualizar la pagina principal")
     public void deberiaVisualizarLaPaginaPrincipal() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(SHORT_WAIT);
+        Serenity.takeScreenshot();
+
         Assert.assertTrue(
                 "El login no fue exitoso",
                 $(lblProducts).waitUntilVisible().isDisplayed()
